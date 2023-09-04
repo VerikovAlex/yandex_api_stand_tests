c51ff87cf6dda785449787f7596eee9f48e93d83
verikov <verikov.alex@gmail.com>
2023-09-04 20:56:50 +0300

    My first commit

---
+++ sender_stand_request.py
@@ -0,0 +1,48 @@
+import configuration
+import requests
+import data
+
+def get_docs():
+    return requests.get(configuration.URL_SERVICE + configuration.DOC_PATH)
+
+def get_logs():
+    return requests.get(configuration.URL_SERVICE + configuration.LOG_MAIN_PATH, params={"count":20})
+
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
+
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  # подставляем полный url
+                         json=body,  # тут тело
+                         headers=data.headers)  # а здесь заголовки
+
+def post_products_kits(body):
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH,  # подставляем полный url
+                         json=body,  # тут тело
+                         headers=data.headers)  # а здесь заголовки
+
+
+#response = post_products_kits(data.product_ids);
+#print(response.status_code)
+#print(response.json())
+
+#response = post_new_user(data.user_body);
+#print(response.status_code)
+#print(response.json())
+
+
+
+#response = post_new_user(data.user_body);
+#print(response.status_code)
+#print(response.json())
+
+#response = get_logs()
+#print(response.status_code)
+#print(response.headers)
+
+#response = get_docs()
+#print(response.status_code)
+
+#response = get_users_table()
+#print(response.status_code)
